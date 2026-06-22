fda4652ab024d67910efe35972008327b4485ae0
Luca <lucagerdau07@gmail.com>
2026-06-22 16:58:32 +0200

    Add cache buster to style.css for footer layout fix

---
+++ fix-css-cache.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const files = [
+    'index.html',
+    'shop.html',
+    'checkout.html',
+    'success.html',
+    'abos.html',
+    'impressum.html',
+    'product-original.html',
+    'product-dark.html',
+    'product-spice.html',
+    'product-classic.html',
+    'lieferant.html'
+];
+
+files.forEach(file => {
+    if (fs.existsSync(file)) {
+        let content = fs.readFileSync(file, 'utf8');
+        content = content.replace(/href="css\/style\.css(\?v=\d+)?"/g, 'href="css/style.css?v=1.0.2"');
+        fs.writeFileSync(file, content, 'utf8');
+    }
+});
+console.log('Fixed CSS cache busting.');
